3c6dd58b64485c25301ecb2e869e73b952962059
Diana <51800713@edu.campus02.at>
2022-11-18 18:32:29 +0100

    renamed files

--- stackCalculator/src/test/java/at/edu/c02/calculator/E2E/EndToEndTest.java
+++ stackCalculator/src/test/java/at/edu/c02/calculator/E2E/EndToEndTest.java
@@ -14,24 +14,24 @@ import static org.mockito.Mockito.*;
 public class EndToEndTest {
 
     @Test
-    public void testParserTest04Xml() throws Exception {
+    public void testParserTest06Xml() throws Exception {
 
         Calculator cal = new CalculatorImpl();
 
         Parser parser = new Parser(cal);
-        double result = parser.parse(new File("src/test/resources/test04.xml"));
+        double result = parser.parse(new File("src/test/resources/test06.xml"));
 
        assertEquals(2,result,0);
 
     }
 
     @Test
-    public void testParserTest05Xml() throws Exception {
+    public void testParserTest07Xml() throws Exception {
 
         Calculator cal = new CalculatorImpl();
 
         Parser parser = new Parser(cal);
-        double result = parser.parse(new File("src/test/resources/test05.xml"));
+        double result = parser.parse(new File("src/test/resources/test07.xml"));
 
         assertEquals(2,result,0);
 
